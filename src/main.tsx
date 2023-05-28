a631741bb034cc72783983359049dc5c28ef3852
Yoshiaki Toyama <77430721+YoshiakiToyama@users.noreply.github.com>
2023-05-29 00:01:32 +0900

    feat: add root and error routes (#5)
    * feat: add route
    
    * feat: add root page
    
    * feat: add error page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
+import Root from './Root';
+import ErrorPage from './ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
